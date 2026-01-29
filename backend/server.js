061ad9bf87ac66ccbcefac562a966b818f05214d
Niyati Sanja <149999123+niyatisanja0206@users.noreply.github.com>
2026-01-29 15:33:52 +0530

    Update CORS origin and session settings in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB(); // MongoDB
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://breast-cancer-treatment-prediction.onrender.com',
   credentials: true
 }));
 
